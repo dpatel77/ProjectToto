e50c48b7d148db9af2ace2eca246f848234bff0d
Dharti Seagraves <dharti.patel.804@gmail.com>
2024-05-06 12:44:59 -0500

    Create project_toto_streamlit.py

---
+++ project_toto_streamlit.py
@@ -0,0 +1,32 @@
+# # Ensure Streamit is installed:
+
+# conda activate base
+# pip install streamlit
+# conda install -c conda-forge streamlit
+
+# # Check if Streamlit is installed:
+
+# streamlit --version
+
+# # Running the Streamlit App:
+
+# streamlit hello
+
+# # Environment Issues:
+# which python
+# which streamlit
+
+# # Reinstall Streamlit:
+
+# pip uninstall streamlit
+# pip install streamlit
+
+
+import streamlit as st
+
+st.write("Project Toto")
+st.write("## Real-Time Intelligent System for Tornado Prediction")
+x = st.text_input("Spring 2024", "Real-Time Intelligent Systems")
+
+if st.button("Click Me"):
+    st.write(f"Your count are you looking for `{x}`")
